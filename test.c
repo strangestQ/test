b0fdd687212e0330969c7aea6cd680d5af72ae93
strangestQ <jimmyoliven@yahoo.co.jp>
2021-10-18 17:22:32 +0900

    测试

---
+++ test.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//
+//		return 0;
+////}
+//int main(void)
+//{
+//	printf("%d\n", 15 - 37);
+////	return 0;
+////}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a, &b);
+//	sum = a + b;
+//	printf("sum = %d\n", sum);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//	     scanf("%d", &arr[i]);
+//	}
+//	int max = arr[0];
+//	for (i = 1; i < 10; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("\nmax=%d\n", max);
+//	return 0;
+//}
+#include<string.h>
+//
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c' };
+//
+//	//int len = strlen("abc");
+//	//printf("%d\n", len);
+//	//printf("%s\n", arr1);
+//	//printf("%s\n", arr2);
+//
+//	return 0;
+//}
