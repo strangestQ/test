5f501b660f8195c74254d3dd0794137d35bde4e5
strangestQ <jimmyoliven@yahoo.co.jp>
2021-10-19 17:11:32 +0900

    宿題

---
+++ homework.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//新明解C言語入門演習1-1
+//int main()
+//{
+//
+//	printf("15から37を引いた値は%dです。\n", 15 - 37);
+//	return 0;
+//}
+//新明解C言語入門演習1-2
+//int main()
+//{
+//	printf("守\n破\n離\n");
+//	return 0;
+//}
+//新明解C言語入門演習1-3
+//int main()
+//{
+//	printf("こんにちは。\nお元気ですか。\n\nさようなら。\n");
+//
+//	return 0;
+//}
