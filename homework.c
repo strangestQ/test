8551dfd109a08a40e2b5305c052ff81e1ffb4374
strangestQ <jimmyoliven@yahoo.co.jp>
2021-10-22 16:18:03 +0900

    練習

--- homework.c
+++ homework.c
@@ -23,9 +23,56 @@
 //	return 0;
 //}
 //新明解C言語入門演習1-4
-int main()
-{
-	int x = 3.14;
-	printf("xの値は%dで。\n", x);
-	return 0;
-}
+//int main()
+//{
+//	int x = 3.14;
+//	printf("xの値は%dで。\n", x);
+//	return 0;
+//}
+//新明解C言語入門演習1-5
+//int main()
+//{
+//	int no;
+//	printf("整数を入力してください：");
+//	scanf("%d",&no);
+//	printf("%dに13を加えると%dです。\n", no, no + 13);
+//	return 0;
+//}
+//新明解C言語入門演習1-6
+//int main()
+//{
+//	int no;
+//	printf("整数を入力してください：");
+//	scanf("%d",&no);
+//	printf("%dから7減じると%dです。\n", no, no - 7);
+//	return 0;
+//}
+//新明解C言語入門演習1-7
+//int main()
+//{
+//	puts("守");
+//	puts("破");
+//	puts("離");
+//	return 0;
+//}
+//新明解C言語入門演習1-8
+//int main()
+//{
+//	int n1, n2;
+//	puts("二つの整数を入力してください。");
+//	printf("整数n1:"); scanf("%d", &n1);
+//	printf("整数n2:"); scanf("%d", &n2);
+//	printf("それらの積は%dです。\n", n1 * n2);
+//	return 0;
+//}
+//新明解C言語入門演習1-9
+//int main()
+//{
+//	int n1, n2, n3;
+//	puts("三つの整数を入力してください。");
+//	printf("整数n1:"); scanf("%d", &n1);
+//	printf("整数n2:"); scanf("%d", &n2);
+//	printf("整数n3:"); scanf("%d", &n3);
+//	printf("それらの和は%dです。\n", n1 + n2 + n3);
+//	return 0;
+//}
